908f52ad0ce53ef99e7f997c071c4043873990df
GList4411 <glist4411@gmail.com>
2024-07-30 14:16:18 -0400

    added error handeling for the file path

--- ER_Dip.py
+++ ER_Dip.py
@@ -1,9 +1,9 @@
 import scipy
 import pandas
 import tkinter
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
-def open_and_read():
+def open_file():
     root = tkinter.Tk() #create root window
     root.withdraw()
 
@@ -11,13 +11,32 @@ def open_and_read():
     file_path = filedialog.askopenfilename(
         title="Select an Excel file", #window title
         filetype=[("Excel files", "*.xlsx *.xls")]) #filters shown files
+
+    return file_path
+
+def read_data(file_path):
+
+    try:
+        #read excel file using "openpyxl"
+        data_frame = pandas.read_excel(file_path, engine="openpyxl")
+        print(data_frame)
+        return data_frame
+    #error handling
+    except FileNotFoundError:
+        messagebox.showerror("File Not Found", "The selected file could not be found.")
+    except PermissionError:
+        messagebox.showerror("Permission Error", "You dont have permission to open the file.")
     
-    #read excel file using "openpyxl"
-    data_frame = pandas.read_excel(file_path, engine="openpyxl")
-    print(data_frame)
-    return data_frame
+    return None
     
 
-open_and_read()
+def main():
+
+    file_path = open_file()
+    if file_path:
+        read_data(file_path)
+
+if __name__ == "__main__":
+    main()
 
 
